ce2050038421e8ab0ed271b4d255dfd2eea91d59
Rory <rorymcgrath@gmail.com>
2026-03-29 15:23:25 -0400

    Add Google avatar support

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  reactStrictMode: true
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      { protocol: "https", hostname: "lh0.googleusercontent.com" },
+      { protocol: "https", hostname: "lh1.googleusercontent.com" },
+      { protocol: "https", hostname: "lh2.googleusercontent.com" },
+      { protocol: "https", hostname: "lh3.googleusercontent.com" },
+      { protocol: "https", hostname: "lh4.googleusercontent.com" },
+      { protocol: "https", hostname: "lh5.googleusercontent.com" },
+      { protocol: "https", hostname: "lh6.googleusercontent.com" }
+    ]
+  }
 };
 
 export default nextConfig;
