79d37b5637291054de65e91227d7a078355173fe
Rory <rorymcgrath@gmail.com>
2026-04-04 10:26:44 -0400

    Add explicit apple touch icon head tags

---
+++ src/app/head.tsx
@@ -0,0 +1,8 @@
+export default function Head() {
+  return (
+    <>
+      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+      <link rel="icon" type="image/png" sizes="512x512" href="/icon.png" />
+    </>
+  );
+}
